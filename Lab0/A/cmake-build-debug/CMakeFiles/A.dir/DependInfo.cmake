5605a2d6417a660335eccfbb7b270983d0b93c46
Aleksey Pauls <aleksey.pauls@mail.ru>
2017-09-17 14:21:33 +0700

    Lab0.A was added

---
+++ Lab0/A/cmake-build-debug/CMakeFiles/A.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "F:/OOP_Cpp/Lab0/A/main.cpp" "F:/OOP_Cpp/Lab0/A/cmake-build-debug/CMakeFiles/A.dir/main.cpp.obj"
+  "F:/OOP_Cpp/Lab0/A/module1.cpp" "F:/OOP_Cpp/Lab0/A/cmake-build-debug/CMakeFiles/A.dir/module1.cpp.obj"
+  "F:/OOP_Cpp/Lab0/A/module2.cpp" "F:/OOP_Cpp/Lab0/A/cmake-build-debug/CMakeFiles/A.dir/module2.cpp.obj"
+  "F:/OOP_Cpp/Lab0/A/module3.cpp" "F:/OOP_Cpp/Lab0/A/cmake-build-debug/CMakeFiles/A.dir/module3.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Lab0/A/cmake-build-debug/CMakeFiles/A.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/A.dir/main.cpp.obj"
+  "CMakeFiles/A.dir/module1.cpp.obj"
+  "CMakeFiles/A.dir/module2.cpp.obj"
+  "CMakeFiles/A.dir/module3.cpp.obj"
+  "A.pdb"
+  "A.exe"
+  "A.exe.manifest"
+  "libA.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/A.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
