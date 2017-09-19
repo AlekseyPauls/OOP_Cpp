82646744050dc4dd2a3bbe8ec2aee993afdc6af4
Aleksey Pauls <aleksey.pauls@mail.ru>
2017-09-19 18:25:23 +0700

    Add changes to Lab0-B

--- Lab0/B/main.cpp
+++ Lab0/B/main.cpp
@@ -4,40 +4,37 @@
 
 using namespace std;
 
-int main(int argc, char* argv[])
-{
-    ifstream fin(argv[1]);
-    ofstream fout(argv[2]);
-    list<string> myList;
-    string myString;
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        cout << "Invalid number of arguments" << endl;
+    }
+    else {
+        ifstream fin(argv[1]);
+        ofstream fout(argv[2]);
+        list<string> myList;
+        string myString;
 
-    if (fin.is_open() && fout.is_open())
-    {
-        /* Reading strings from input file to list container */
-        while (!fin.eof())
-        {
-            getline(fin, myString);
-            myList.push_back(myString);
+        if (fin.is_open() && fout.is_open()) {
+            /* Reading strings from input file to list container */
+            while (!fin.eof()) {
+                getline(fin, myString);
+                myList.push_back(myString);
+            }
+
+            /* Sorting strings */
+            Module1::sort_strings(&myList);
+
+            /* Writing strings from sorted list to output file */
+            list<string>::iterator it;
+            for (auto& it : myList) {
+                fout << it << endl;
+            }
+
+            fin.close();
+            fout.close();
+        } else {
+            cout << "Can`t open the file";
         }
-
-        /* Sorting strings */
-        Module1::sort_strings(&myList);
-
-        /* Writing strings from sorted list to output file */
-        list<string>::iterator it;
-        for (it = myList.begin(); it != myList.end(); it++)
-        {
-            fout << *it << endl;
-        }
-
-        fin.close();
-        fout.close();
-    } else
-    {
-        cout << "Can`t open the file";
-
-        fin.close();
-        fout.close();
     }
 
     return 0;
