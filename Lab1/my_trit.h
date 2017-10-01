4b423745467552dc6a947c1f80bdf6fb31a2c3f0
Aleksey Pauls <aleksey.pauls@mail.ru>
2017-10-01 23:52:06 +0700

    Lab1

---
+++ Lab1/my_trit.h
@@ -0,0 +1,12 @@
+
+
+class Trit {
+private:
+    uint *trit_ptr; // Указатель на начало
+    int size; // Размер
+    uint last; // Последний элемент
+public:
+    Trit(); // Конструктор
+    ~Trit(); // Деструктор
+
+};
