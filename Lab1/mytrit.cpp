8199522c7b97fca828a2673499dd5f6b9ef1a0a2
Aleksey Pauls <aleksey.pauls@mail.ru>
2017-10-02 20:14:13 +0700

    Add

--- Lab1/mytrit.cpp
+++ Lab1/mytrit.cpp
@@ -4,3 +4,7 @@ TritSet::TritSet(int set_size) {
     TritSet::begin = (uint*) malloc(sizeof(uint)*(ceil((double)set_size * 2 / 8 / sizeof(uint))));
     TritSet::size = set_size;
 }
+
+TritSet::~TritSet() {
+    free(TritSet::begin);
+}
